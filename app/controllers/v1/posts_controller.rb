0fe1918c5331dfbdd029fb6d8b49f72b71bede5d
3guna3 <83042016+3guna3@users.noreply.github.com>
2021-11-20 15:01:33 +0900

    showアクションを編集

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -4,11 +4,19 @@ class V1::PostsController < ApplicationController
     render json: posts
   end
 
-  def show; end
+  def show
+    render json: @post
+  end
 
   def create; end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
